79cfb9f21862360278df63d3bf6e4e554b3b2279
Azerlus <s.vermeulen57@gmail.com>
2018-05-15 00:10:17 +0200

    Insertion de tinyMCE + modifications des vues insertion et update des chapitres

--- App/Backend/Modules/Chapters/Views/_form.php
+++ App/Backend/Modules/Chapters/Views/_form.php
@@ -1,19 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=3miwn5u5g5p2ialx54r0uiwkn8h1c72el4b9shdzy8vf78t8"></script>
+  <script>tinymce.init({ selector:'textarea' });</script>
+
 <form action="" method="post">
   <p>
-    <?= isset($erreurs) && in_array(\Entity\News::AUTEUR_INVALIDE, $erreurs) ? 'L\'auteur est invalide.<br />' : '' ?>
+    <?= isset($erreurs) && in_array(\Entity\Chapters::INVALID_AUTHOR, $erreurs) ? 'L\'auteur est invalide.<br />' : '' ?>
     <label>Auteur</label>
-    <input type="text" name="auteur" value="<?= isset($news) ? $news['auteur'] : '' ?>" /><br />
+    <input type="text" name="auteur" value="<?= isset($chapters) ? $chapters['auteur'] : '' ?>" /><br />
  
-    <?= isset($erreurs) && in_array(\Entity\News::TITRE_INVALIDE, $erreurs) ? 'Le titre est invalide.<br />' : '' ?>
-    <label>Titre</label><input type="text" name="titre" value="<?= isset($news) ? $news['titre'] : '' ?>" /><br />
+    <?= isset($erreurs) && in_array(\Entity\Chapters::INVALID_TITLE, $erreurs) ? 'Le titre est invalide.<br />' : '' ?>
+    <label>Titre</label><input type="text" name="titre" value="<?= isset($chapters) ? $chapters['titre'] : '' ?>" /><br />
  
-    <?= isset($erreurs) && in_array(\Entity\News::CONTENU_INVALIDE, $erreurs) ? 'Le contenu est invalide.<br />' : '' ?>
-    <label>Contenu</label><textarea rows="8" cols="60" name="contenu"><?= isset($news) ? $news['contenu'] : '' ?></textarea><br />
+    <?= isset($erreurs) && in_array(\Entity\Chapters::INVALID_CONTENT, $erreurs) ? 'Le contenu est invalide.<br />' : '' ?>
+    <label>Contenu</label><textarea rows="8" cols="60" name="contenu"><?= isset($chapters) ? $chapters['contenu'] : '' ?></textarea><br />
 <?php
-if(isset($news) && !$news->isNew())
+if(isset($chapters) && !$chapters->isNew())
 {
 ?>
-    <input type="hidden" name="id" value="<?= $news['id'] ?>" />
+    <input type="hidden" name="id" value="<?= $chapters['id'] ?>" />
     <input type="submit" value="Modifier" name="modifier" />
 <?php
 }
